18a46a736cd2f6cbec62ceebef874a27b47da032
budimanlai <budiman.lai@gmail.com>
2022-07-02 12:45:21 +0700

    fix build app name

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,7 +3,7 @@
 GIT=`git log | head -n 1 | cut  -f 2 -d ' ' | head -c 8`
 
 MAIN="cmd/*.go"
-APP="sgb-register"
+APP="mail-queue"
 FILE="dist/$APP-$GIT"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ${FILE} $MAIN
